66b6d4726c7aa3383db214d447f2f298d727bfba
Abdurahmon <hallaymstore@gmail.com>
2026-09-21 17:06:32 +0500

    Refresh PWA cache for OAuth control v2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ytshield-v1';
+const CACHE='ytshield-v2';
 const CORE=['/','/styles.css','/app.js','/icon.svg','/manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
